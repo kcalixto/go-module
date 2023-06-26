70f867f92107de8721715ad229d551ed1d1930e5
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-06-26 00:11:59 -0300

    created first toolkit function

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -1,3 +1,26 @@
 package toolkit
 
+import (
+	"crypto/rand"
+)
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
 type Tools struct{}
+
+func (t *Tools) RandomString(length int) string {
+	s := make([]rune, length)
+	r := []rune(randomStringSource)
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+
+		x := p.Uint64()
+
+		y := uint64(len(r))
+
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
